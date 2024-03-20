ac3d6b87e3256b087101003a0bda5dc9b78ca475
vivekperbhoo <perbhoovivek@gmail.com>
2024-03-20 18:59:04 -0400

    Added login page

--- CSI2532Projet/Client/src/App.tsx
+++ CSI2532Projet/Client/src/App.tsx
@@ -1,8 +1,17 @@
 import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+
 import { ChakraProvider } from "@chakra-ui/react";
+import Login from "./Login";
 
 function App() {
-  return <ChakraProvider></ChakraProvider>;
+  return (
+    <Router>
+      <Routes>
+        <Route path = "/" element = {<Login />}></Route>
+      </Routes>
+    </Router>
+  );
 }
 
 export default App;
---
+++ CSI2532Projet/Client/src/Login.tsx
@@ -0,0 +1,15 @@
+import "./Login.css"
+
+function Login(){
+    return(
+    <>
+        <body>
+            <div className="img-container"></div>
+            <div className="transparent-film"></div>
+            <div className="login-container"></div>
+        </body>
+    </>
+    );
+}
+
+export default Login

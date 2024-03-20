d13dcf5982f9fd25d846f4ff848dbf23a2c71825
tanyapyndiah <tppyndiah@me.com>
2024-03-20 18:47:51 -0400

    roomicon

--- CSI2532Projet/Client/src/components/RoomIcon/RoomIcon.tsx
+++ CSI2532Projet/Client/src/components/RoomIcon/RoomIcon.tsx
@@ -1,3 +1,5 @@
+import { Link } from "react-router-dom";
+
 interface RoomIconParam {
     image: string;
     hotelName:string;
@@ -8,15 +10,16 @@ interface RoomIconParam {
 
 function RoomIcon({image,hotelName,roomNumber,price,numberPeople}:RoomIconParam){
     return (
-        <div className="icon" onClick={()=>handleClick()}>
-            <img className="roomImage" src={image}/>
-        
-            <div className="text">
-                <p className="hotelName">{hotelName}</p>
-                <p className="roomNumber">{roomNumber}</p>
-                <p className="price">{price}</p>
-                <p className="numberPeople">{numberPeople}</p>
+        <Link to="/roomPage">
+            <div className="icon">
+                <img className="roomImage" src={image}/>
+                <div className="text">
+                    <p className="hotelName">{hotelName}</p>
+                    <p className="roomNumber">{roomNumber}</p>
+                    <p className="price">{price}</p>
+                    <p className="numberPeople">{numberPeople}</p>
+                </div>
             </div>
-        </div>
+        </Link>
     );
 }

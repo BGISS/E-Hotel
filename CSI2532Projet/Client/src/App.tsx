bd5df6bfa015f946b4334da308eedd1c71ff3a1d
BGISS <bissgirish22@gmail.com>
2024-03-21 00:53:00 -0400

    Added EmployeePage

--- CSI2532Projet/Client/src/App.tsx
+++ CSI2532Projet/Client/src/App.tsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route path = "/" element = {<CustomerSearchPage />}></Route>
+        <Route path="/" element={<CustomerSearchPage />}></Route>
       </Routes>
     </Router>
   );
--- CSI2532Projet/Client/src/EmployeePage.tsx
+++ CSI2532Projet/Client/src/EmployeePage.tsx
@@ -2,12 +2,21 @@ import EmployeeCard from "./components/EmployeeCard/EmployeeCard";
 import hotelIcon from "./assets/ic_round-hotel.png";
 import locationIcon from "./assets/carbon_stay-inside.png";
 import cashIcon from "./assets/mdi_account-cash.png";
+import hotelImage from "./assets/Employee image.jpeg";
+import "./EmployeePage.css";
 function EmployeePage() {
   return (
     <>
-      <EmployeeCard icon={hotelIcon} text={"Reservation"}></EmployeeCard>
-      <EmployeeCard icon={locationIcon} text={"Locations"}></EmployeeCard>
-      <EmployeeCard icon={cashIcon} text={"Inserer payment"}></EmployeeCard>
+      <div className="image-div">
+        <img className="image-hotel" src={hotelImage}></img>
+      </div>
+      <div className="cards">
+        <EmployeeCard icon={hotelIcon} text={"Reservation"}></EmployeeCard>
+        <EmployeeCard icon={locationIcon} text={"Locations"}></EmployeeCard>
+        <EmployeeCard icon={cashIcon} text={"Inserer payment"}></EmployeeCard>
+      </div>
     </>
   );
 }
+
+export default EmployeePage;
